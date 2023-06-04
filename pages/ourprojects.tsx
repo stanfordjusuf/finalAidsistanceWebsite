294f74183b1a45a9e6348c64acebbc79fe02c87e
stanfordjusuf <107454100+stanfordjusuf@users.noreply.github.com>
2023-06-04 18:09:36 +0700

    Update ourprojects.tsx

--- pages/ourprojects.tsx
+++ pages/ourprojects.tsx
@@ -1,16 +1,16 @@
-import React from "react";
+import React from 'react';
 
 export default function ourprojects() {
   return (
     <>
-      <center style={{ backgroundColor: "#ec6f7c" }}>
+      <center style={{ backgroundColor: '#ec6f7c' }}>
         <br />
         <h1 className="text-5xl font-bold text-black">Our Projects & Events</h1>
         <br />
       </center>
       <div
         className="hero min-h-screen bg-base-200"
-        style={{ backgroundColor: "#faf9f6" }}
+        style={{ backgroundColor: '#faf9f6' }}
       >
         <div className="hero-content flex-col lg:flex-row-reverse">
           <div className="carousel carousel-center max-w-md p-4 space-x-4 bg-neutral rounded-box">
