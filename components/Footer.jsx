a53d7d2834f26eda3bfa06a0f218b9557dea8a3c
stanfordjusuf <107454100+stanfordjusuf@users.noreply.github.com>
2023-06-04 18:16:26 +0700

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import Link from "next/link";
+import React from 'react';
+
 
 const Footer = () => (
   <footer
     className="footer footer-center p-10 bg-primary text-primary-content"
-    style={{ backgroundColor: "#faf9f6" }}
+    style={{ backgroundColor: '#faf9f6' }}
   >
     <div>
       <img
         src="https://i.ibb.co/8r3L1mC/Logo-2.png"
         alt=""
-        style={{ width: "30%", height: "auto" }}
+        style={{ width: '30%', height: 'auto' }}
       />
       <p className="font-bold">
         Sponsored By <br />
@@ -20,22 +20,22 @@ const Footer = () => (
     <div>
       <div className="grid grid-flow-col gap-4">
         <center>
-          <button className="btn btn-ghost btn-circle">
+          <button type="button" className="btn btn-ghost btn-circle">
             <a href="https://www.instagram.com/maxleysuites/">
               <img
                 src="https://i.ibb.co/d2vBPhL/1.png"
                 alt=""
-                style={{ width: "20px", height: "auto" }}
+                style={{ width: '20px', height: 'auto' }}
               />
             </a>
           </button>
-          <button className="btn btn-ghost btn-circle">
+          <button type="button" className="btn btn-ghost btn-circle">
             <div className="indicator">
               <a href="mailto:maxleysuites@gmail.com">
                 <img
                   src="https://i.ibb.co/ChdTZp1/5.png"
                   alt=""
-                  style={{ width: "20px", height: "auto" }}
+                  style={{ width: '20px', height: 'auto' }}
                 />
               </a>
             </div>
