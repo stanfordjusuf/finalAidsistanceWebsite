baa3286849bcc4844a1592da5d640abd0ae0e1dd
stanfordjusuf <107454100+stanfordjusuf@users.noreply.github.com>
2023-06-04 18:04:31 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
-import React from "react";
-import "../styles/globals.scss";
-import { Layout } from "../components";
+import React from 'react';
+import '../styles/globals.scss';
+import { Layout } from '../components';
 
 function MyApp({ Component, pageProps }) {
   return (
