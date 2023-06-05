a8ecea1cd5998df8d4e964dfc939318aea3f1455
stanfordjusuf <107454100+stanfordjusuf@users.noreply.github.com>
2023-06-05 20:21:56 +0700

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -20,7 +20,7 @@ const Footer = () => (
       <div className="grid grid-flow-col gap-4">
         <center>
           <button type="button" className="btn btn-ghost btn-circle">
-            <a href="https://www.instagram.com/maxleysuites/">
+            <a href="https://www.instagram.com/aidsistance/?igshid=NTc4MTIwNjQ2YQ==">
               <img
                 src="https://i.ibb.co/d2vBPhL/1.png"
                 alt=""
