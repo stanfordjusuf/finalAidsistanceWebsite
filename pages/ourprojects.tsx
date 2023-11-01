31bef6de6a6c593a9efdde39a87b777a7752f8b8
stanfordjusuf <107454100+stanfordjusuf@users.noreply.github.com>
2023-11-01 12:00:50 +0700

    Update ourprojects.tsx

--- pages/ourprojects.tsx
+++ pages/ourprojects.tsx
@@ -16,7 +16,7 @@ export default function ourprojects() {
           <div className="carousel carousel-center max-w-md p-4 space-x-4 bg-neutral rounded-box">
             <div className="carousel-item">
               <img
-                src="https://i.ibb.co/DkkcPFk/Screenshot-2023-11-01-at-11-55-47.png"
+                src="https://i.ibb.co/2cPQYmm/Screenshot-2023-11-01-at-11-58-41.png"
                 className="rounded-box"
               />
             </div>
